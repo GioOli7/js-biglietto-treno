da1a8c424f8c2f3038e7e306ce5a57bd34d0dc47
GioOli7 <gio07live@gmail.com>
2021-03-19 15:49:58 +0100

    base format

--- js/script.js
+++ js/script.js
@@ -14,10 +14,12 @@ var scontoOver65 = 0.4;
 
 // chiedo km da percorrere
 var kmViaggio = parseInt( prompt("Quanti chilometri deve percorrere") );
+document.getElementById('km-viaggio').innerHTML = kmViaggio; 
 console.log("Km da percorrere: ", kmViaggio);
 
 // chiedo età del passeggero
 var age = parseInt( prompt("Indichi la sua età") );
+document.getElementById('age-passenger').innerHTML = age; 
 console.log("Età del passeggero: ", age);
 
 // calcolo il prezzo del biglietto standard
@@ -26,10 +28,20 @@ var price = kmViaggio * priceKm;
 // apporto gli sconti se necessario
 if (age < 18) {
     price -= price * scontoUnder18;
+    // formatto l'output del prezzo
+    var formatPrice = price.toFixed(2) + ' euro';
+    document.getElementById('prezzo-biglietto').innerHTML = formatPrice;
     console.log("Prezzo scontato per Under18: ", price);
+
 } else if (age >= 65){
     price -= price * scontoOver65;
+    // formatto l'output del prezzo
+    var formatPrice = price.toFixed(2) + ' euro';
+    document.getElementById('prezzo-biglietto').innerHTML = formatPrice;
     console.log("Prezzo scontato per Over65: ", price);
 } else {
+    // formatto l'output del prezzo
+    var formatPrice = price.toFixed(2) + ' euro';
+    document.getElementById('prezzo-biglietto').innerHTML = formatPrice;
     console.log("Prezzo del biglietto standard: ", price);
 }
