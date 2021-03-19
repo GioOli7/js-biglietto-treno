5b732e0e67d23ee1ca5aba6b5a6ec13ae6349db1
GioOli7 <gio07live@gmail.com>
2021-03-19 16:01:41 +0100

    data validation

--- js/script.js
+++ js/script.js
@@ -7,32 +7,45 @@
 // se l'età del passeggero è superiore ai 65, applico sconto del 40%
 // stampo risultato con massimo due decimali
 
-// Prezzo biglietto treno e sconti
+
+// PREZZO BIGLIETTO E SCONTI
 var priceKm = 0.21;
 var scontoUnder18 = 0.2;
 var scontoOver65 = 0.4;
 
-// chiedo km da percorrere
+
+// CHIEDO KM DA PERCORRERE
 var kmViaggio = parseInt( prompt("Quanti chilometri deve percorrere") );
+// servirebbe un loop, ma non ci siamo ancora arrivati
+// se il dato non è numerico, faccio ripetere l'inserimento dati
+if (isNaN(kmViaggio) || kmViaggio < 0) {
+    kmViaggio = parseInt( prompt("Inserire un dato numerico valido") );
+}
 document.getElementById('km-viaggio').innerHTML = kmViaggio; 
 console.log("Km da percorrere: ", kmViaggio);
 
-// chiedo età del passeggero
+
+// CHIEDO ETÀ DEL PASSEGGERO
 var age = parseInt( prompt("Indichi la sua età") );
+// se il dato non è numerico, faccio ripetere l'inserimento dati
+if (isNaN(age) || age < 0) {
+    age = parseInt( prompt("Inserire un dato numerico valido") );
+}
 document.getElementById('age-passenger').innerHTML = age; 
 console.log("Età del passeggero: ", age);
 
-// calcolo il prezzo del biglietto standard
+
+// CALCOLO PREZZO STANDARD
 var price = kmViaggio * priceKm;
 
-// apporto gli sconti se necessario
+
+// APPORTO SCONTI SE NECESSARIO
 if (age < 18) {
     price -= price * scontoUnder18;
     // formatto l'output del prezzo
     var formatPrice = price.toFixed(2) + ' euro';
     document.getElementById('prezzo-biglietto').innerHTML = formatPrice;
     console.log("Prezzo scontato per Under18: ", price);
-
 } else if (age >= 65){
     price -= price * scontoOver65;
     // formatto l'output del prezzo
